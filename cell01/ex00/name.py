6b849ce11f3f5d76db573a2724910307c0466223
Kmitl68070131 <68070131@kmitl.ac.th>
2026-09-22 00:53:24 +0700

    cell01

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Pakawat"
+last_name  = "Hunluangrach"
+print(f"{first_name} {last_name}")
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Pakawat"
+last_name  = "Hunluangrach"
+whole_name = first_name + " " + last_name
+print(whole_name)
